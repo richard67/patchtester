d2789bbca46e667872f68acddf032b86e9582c10
Roland Dalmulder <contact@rolandd.com>
2020-03-31 20:20:15 +0200

    Cleanup the SQL files #234

--- administrator/components/com_patchtester/install/sql/mysql/uninstall.sql
+++ administrator/components/com_patchtester/install/sql/mysql/uninstall.sql
@@ -1,3 +1,2 @@
 DROP TABLE IF EXISTS `#__patchtester_pulls`;
 DROP TABLE IF EXISTS `#__patchtester_tests`;
-DROP TABLE IF EXISTS `#__patchtester_chain`;
--- administrator/components/com_patchtester/install/sql/updates/postgresql/4.0.0.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "#__patchtester_chain" (
-  "id" serial NOT NULL,
-  "insert_id" bigint NOT NULL,
-  "pull_id" bigint NOT NULL,
-  PRIMARY KEY ("id")
-);
