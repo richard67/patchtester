dd45425fdcbc7936fb7660f472d492e4b0c2d646
Matt Meyrick <matt_meyrick@btinternet.com>
2020-04-04 22:04:07 +0100

    Update script.php

--- administrator/components/com_patchtester/script.php
+++ administrator/components/com_patchtester/script.php
@@ -51,24 +51,24 @@ class Com_PatchtesterInstallerScript extends InstallerScript
 	 */
 	public function postflight($type, $parent)
 	{
-		if ($type == 'install') 
-			{
-					$language = JFactory::getLanguage();
-					$language->load('com_patchtester', JPATH_ADMINISTRATOR, null, true);
-					$language->load('com_patchtester', JPATH_SITE, null, true);
-					echo '<h1><img src="../media/com_patchtester/images/icon-48-patchtester.png"> ' . Text::_('COM_PATCHTESTER') . '</h1>';
-					echo '<p>' . Text::_('COM_PATCHTESTER_XML_DESCRIPTION') . '</p>';
-					echo '<p>' . Text::_('COM_PATCHTESTER_GOTO') . ' <a href="index.php?option=com_patchtester">' . Text::_('COM_PATCHTESTER') . '</a></p>';
-					echo '<p>' . Text::_('COM_PATCHTESTER_GOTO') . ' <a href="index.php?option=com_config&view=component&component=com_patchtester#authentication	&path=&return=aHR0cDovL2xvY2FsaG9zdC9wYXRjaDRhMTIvYWRtaW5pc3RyYXRvci9pbmRleC5waHA%2Fb3B0aW9uPWNvbV9wYXRjaHRlc3Rlcg%3D%3D">' . Text::_('COM_PATCHTESTER_OPTIONS') . '</a></p>';
-			}
-	
-		if ($type == 'uninstall') 
-			{
-					$language = JFactory::getLanguage();
-					$language->load('com_patchtester', JPATH_ADMINISTRATOR, null, true);
-					$language->load('com_patchtester', JPATH_SITE, null, true);
-					echo '<h1>' . Text::_('COM_PATCHTESTER') . '</h1>';
-					echo '<p>' . Text::_('COM_PATCHTESTER_UNINSTALL_THANK_YOU') . '</p>';
-			}
+		if ($type == 'install')
+		{
+				$language = JFactory::getLanguage();
+				$language->load('com_patchtester', JPATH_ADMINISTRATOR, null, true);
+				$language->load('com_patchtester', JPATH_SITE, null, true);
+				echo '<h1><img src="../media/com_patchtester/images/icon-48-patchtester.png"> ' . Text::_('COM_PATCHTESTER') . '</h1>';
+				echo '<p>' . Text::_('COM_PATCHTESTER_XML_DESCRIPTION') . '</p>';
+				echo '<p>' . Text::_('COM_PATCHTESTER_GOTO') . ' <a href="index.php?option=com_patchtester">' . Text::_('COM_PATCHTESTER') . '</a></p>';
+				echo '<p>' . Text::_('COM_PATCHTESTER_GOTO') . ' <a href="index.php?option=com_config&view=component&component=com_patchtester#authentication	&path=&return=aHR0cDovL2xvY2FsaG9zdC9wYXRjaDRhMTIvYWRtaW5pc3RyYXRvci9pbmRleC5waHA%2Fb3B0aW9uPWNvbV9wYXRjaHRlc3Rlcg%3D%3D">' . Text::_('COM_PATCHTESTER_OPTIONS') . '</a></p>';
+		}
+
+		if ($type == 'uninstall')
+		{
+				$language = JFactory::getLanguage();
+				$language->load('com_patchtester', JPATH_ADMINISTRATOR, null, true);
+				$language->load('com_patchtester', JPATH_SITE, null, true);
+				echo '<h1>' . Text::_('COM_PATCHTESTER') . '</h1>';
+				echo '<p>' . Text::_('COM_PATCHTESTER_UNINSTALL_THANK_YOU') . '</p>';
+		}
 	}
 }
